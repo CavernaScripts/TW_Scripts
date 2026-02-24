dc73845de0e78700b757af9ae94c0fce1d750168
Luiz Fernando da Silva DA SILVA <lfernandosil@gmail.com>
2026-02-24 09:17:40 -0300

    Create FarmA.js

---
+++ FarmA.js
@@ -0,0 +1,9 @@
+javascript:
+var arr = $("a.farm_icon_a")
+        , i = 0
+        , t = setInterval(function ()
+        {
+                $(arr[i++])
+                        .click();
+                if (i > arr.length) clearInterval(t);
+        }, 400);
