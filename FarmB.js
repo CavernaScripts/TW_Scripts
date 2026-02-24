b10bd52e4d06e4e5b75921a6ac2202d6911aa2d7
Luiz Fernando da Silva DA SILVA <lfernandosil@gmail.com>
2026-02-24 09:24:27 -0300

    Create FarmB.js

---
+++ FarmB.js
@@ -0,0 +1,9 @@
+javascript:
+var arr = $("a.farm_icon_b")
+        , i = 0
+        , t = setInterval(function ()
+        {
+                $(arr[i++])
+                        .click();
+                if (i > arr.length) clearInterval(t);
+        }, 400);
